1f518dcb43b573c04871f4ee126c239d4d115d65
Justarski <Justarski99@gmail.com>
2024-12-07 20:11:50 -0500

    Mitosis and Evolution
    Mitosis and Evolution functionality and UI.

--- Assets/Scripts/CellPanel.cs
+++ Assets/Scripts/CellPanel.cs
@@ -4,13 +4,21 @@ using TMPro;
 
 public class CellPanel : MonoBehaviour
 {
+    [SerializeField] MitosisHandler mitosisHandler;
     [SerializeField] GameObject organellePanel;
     [SerializeField] Button evolveButton;
+    [SerializeField] TextMeshProUGUI MitosisCostText;
+    [SerializeField] TextMeshProUGUI  mitosisDNACostText;
+    [SerializeField] TextMeshProUGUI  mitosisProteinCostText;
+    [SerializeField] TextMeshProUGUI  mitosisATPCostText;
     bool evolveToggle = false;
 
     void Start()
     {
         gameObject.SetActive(false);
+        mitosisDNACostText.text = "DNA: " + mitosisHandler.GetMitosisDNACost();
+        mitosisProteinCostText.text = "Protein: " + mitosisHandler.GetMitosisDNACost();
+        mitosisATPCostText.text = "ATP: " + mitosisHandler.GetMitosisDNACost();
     }
 
     public void OnCellClick()
@@ -29,11 +37,25 @@ public class CellPanel : MonoBehaviour
         {
             buttonText.text = "Evolve";
             buttonImage.color = Color.green;
+
+            MitosisCostText.text = "Mitosis and Evolution Cost:";
+            mitosisDNACostText.text = "DNA: " + (mitosisHandler.GetMitosisDNACost() + mitosisHandler.GetEvolveDNACost());
+            mitosisProteinCostText.text = "Protein: " + (mitosisHandler.GetMitosisProteinCost() + mitosisHandler.GetEvolveProteinCost());
+            mitosisATPCostText.text = "ATP: " + (mitosisHandler.GetMitosisATPCost() + mitosisHandler.GetEvolveATPCost());
+
+            mitosisHandler.ToggleEvolve();
         }
         else
         {
             buttonText.text = "Evolve?";
             buttonImage.color = Color.red;
+
+            MitosisCostText.text = "Mitosis Cost:";
+            mitosisDNACostText.text = "DNA: " + mitosisHandler.GetMitosisDNACost();
+            mitosisProteinCostText.text = "Protein: " + mitosisHandler.GetMitosisProteinCost();
+            mitosisATPCostText.text = "ATP: " + mitosisHandler.GetMitosisATPCost();
+
+            mitosisHandler.ToggleEvolve();
         }
     }
 
